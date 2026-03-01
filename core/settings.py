f9b7919afc8e20722aad0506b94319b189bbe938
Streakdethird <bamideledaniel93@gmail.com>
2026-03-01 18:36:25 +0100

    Fix DB config (SQLite local, DATABASE_URL prod)

--- core/settings.py
+++ core/settings.py
@@ -87,7 +87,7 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     "default": dj_database_url.config(
-        default="",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
     )
 }
